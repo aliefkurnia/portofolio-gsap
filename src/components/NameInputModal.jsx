6a2ec5a59f7638c7b15654b532ac5f448f54f674
Muchammad Alief Kurnia Wijaya <aliefkw26@gmail.com>
2024-12-13 11:26:13 +0700

    fixhero

--- src/components/NameInputModal.jsx
+++ src/components/NameInputModal.jsx
@@ -4,13 +4,41 @@ import "./NameInputModal.css";
 const NameInputModal = ({ onSubmit }) => {
   const [name, setName] = useState("");
   const [error, setError] = useState("");
-  const inputRef = useRef(null); 
+  const inputRef = useRef(null);
+  const modalRef = useRef(null);
 
   useEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus();
     }
+
+    const allFocusableElements = document.querySelectorAll(
+      'button, input, select, textarea, a, [tabindex]:not([tabindex="-1"])'
+    );
+    
+    const modalFocusableElements = modalRef.current.querySelectorAll(
+      'button, input, select, textarea'
+    );
+
+    allFocusableElements.forEach((el) => {
+      el.setAttribute("tabindex", "-1");
+    });
+
+    modalFocusableElements.forEach((el) => {
+      el.setAttribute("tabindex", "0");
+    });
+
+    return () => {
+      allFocusableElements.forEach((el) => {
+        el.removeAttribute("tabindex");
+      });
+
+      modalFocusableElements.forEach((el) => {
+        el.removeAttribute("tabindex");
+      });
+    };
   }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name.trim() && name.length <= 10) {
@@ -24,7 +52,7 @@ const NameInputModal = ({ onSubmit }) => {
     const inputName = e.target.value;
     if (inputName.length <= 10) {
       setName(inputName);
-      setError(""); 
+      setError("");
     } else {
       setError("Name must be less than 10 characters.");
     }
@@ -32,7 +60,7 @@ const NameInputModal = ({ onSubmit }) => {
 
   return (
     <div className="modal-overlay">
-      <div className="modal-content">
+      <div className="modal-content" ref={modalRef}>
         <h2>What's your Nickname?</h2>
         <form onSubmit={handleSubmit}>
           <input
@@ -40,11 +68,11 @@ const NameInputModal = ({ onSubmit }) => {
             placeholder="example: agus"
             value={name}
             onChange={handleChange}
-            ref={inputRef} 
+            ref={inputRef}
           />
           {error && <p className="error-messageModal">{error}</p>}
           <button type="submit" disabled={!name.trim() || name.length > 10}>
-            Lets Go
+            Let's Go
           </button>
         </form>
       </div>
